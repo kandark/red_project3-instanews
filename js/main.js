d6c82398c51ac74af5e33713b8868d45985b77a4
kanda <rodrigokanda2016@gmail.com>
2020-02-20 14:57:10 -0500

    fixing...

--- js/main.js
+++ js/main.js
@@ -1,34 +1,35 @@
 
 // Menu function
-$(function() {
-    $("#select").on("change", function(event) {
-      const $selected = $(event.target).val();
-   
-      const section = $("#sections").val();
-      $("header").addClass("mover");
-      //console.log(`You have selected ${$selected}`)
-     $.ajax({
-        method: 'GET',
-        url: `https://api.nytimes.com/svc/topstories/v2/${$selected}.json?api-key=4hmoxMS85CtrYORgACww50nTW7xoRIKr`
-    
-    }).done(function({results}){
-        console.log(results)
-        $.each(results,function(index,value){
-        
+$(function () {
+  $("#select").on("change", function (event) {
+    const $selected = $(event.target).val();
 
-            
-        });
+    const section = $("#sections").val();
+    $("header").addClass("mover");
+    $(".loader-gif").css("display", "block");
+    $(".loader-gif").addClass("loader-gif2");
+    //console.log(`You have selected ${$selected}`)
+    $.ajax({
+      method: 'GET',
+      url: `https://api.nytimes.com/svc/topstories/v2/${$selected}.json?api-key=4hmoxMS85CtrYORgACww50nTW7xoRIKr`
 
-        for (i=0; i< 12; i++) {
-            let abstract = results[i].abstract;
-            let url = results[i].url;
+    }).done(function ({ results }) {
+      $(".loader-gif").fadeOut(1250);
+      console.log(results)
+      $.each(results, function (index, value) {
+      });
 
-      let image =  results[i].multimedia[0].url
-      console.log(image)
-      console.log(abstract);
-      console.log(url);
-      $("#multimedias").append(
-        `
+      for (i = 0; i < 12; i++) {
+        let abstract = results[i].abstract;
+        let url = results[i].url;
+
+        let image = results[i].multimedia[0].url
+        console.log(image)
+        console.log(abstract);
+        console.log(url);
+        setTimeout(() => {
+          $("#multimedias").append(
+            `
         <a  class="link" href="${url}">
         <figure>
        
@@ -37,16 +38,17 @@ $(function() {
       </figure>
             </a> 
         `
-      );
-        }
+          );
+        }, 1500);
 
-        
+      }
 
 
-        })
-    
+
+
+    })
+
+  })
 })
- })
 
 
- 
